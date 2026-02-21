bad14a4c2db2cc74b1f2dd264739d522ba7a5b51
Waruenat Rabiabporn <waruenatrabiabporn@gmail.com>
2026-02-21 20:39:34 +0700

    add contact page

--- app.py
+++ app.py
@@ -12,3 +12,7 @@ if __name__ == '__main__':
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')

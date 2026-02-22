512244034d62b290ca38cc4cbd22ea3dad45a5d7
Waruenat Rabiabporn <waruenatrabiabporn@gmail.com>
2026-02-22 20:35:37 +0700

    configure sqlite database

--- app.py
+++ app.py
@@ -16,3 +16,12 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///food.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)

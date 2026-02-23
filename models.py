a371096c166313f3616e6f373d10765bc6d7e1ab
Waruenat Rabiabporn <waruenatrabiabporn@gmail.com>
2026-02-23 17:44:08 +0700

    create Restaurant model

---
+++ models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Restaurant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    location = db.Column(db.String(200))
+    description = db.Column(db.Text)
+
+    def __repr__(self):
+        return f'<Restaurant {self.name}>'

359d7f6be2fb1b7bb12988c91a81a62347ffeb56
Waruenat Rabiabporn <waruenatrabiabporn@gmail.com>
2026-02-28 21:48:24 +0700

    add user model with password hashing

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 import os
 
 # --------------------
@@ -34,8 +35,6 @@ class Restaurant(db.Model):
         if not self.reviews:
             return None
         return round(sum(r.rating for r in self.reviews) / len(self.reviews), 1)
-
-
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     rating = db.Column(db.Integer, nullable=False)
@@ -44,6 +43,17 @@ class Review(db.Model):
         db.ForeignKey("restaurant.id"),
         nullable=False
     )
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 # --------------------
 # Routes
